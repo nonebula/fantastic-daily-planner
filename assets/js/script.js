333ddd0925b8310182430bc0d61d7e53003d87e6
nonebula <147166151+nonebula@users.noreply.github.com>
2023-12-08 19:30:06 +0000

    Added rows via jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,21 @@ var saveButton = $("#save-button");
 var completeButton = $("#complete-button");
 var clearButton = $("#clear-button");
 
+// table body content
+$(document).ready(function () {
+var businessHours = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]
+
+var tbody = $('.time-table tbody');
+
+for (var i = 0; i < businessHours.length; i++) {
+    var newRow = $('<tr>');
+    newRow.append('<td class="time-block">' + businessHours[i] + '</td>');
+    newRow.append('<td class="col-md-7 add-content"><textarea></textarea></td>');
+    newRow.append('<td class="col-md-1 save-block"><button id ="save-button" class="btn btn-primary">Save</button></td>');
+    newRow.append('<td class="col-md-1 complete-block"><button id ="complete-button" class="btn btn-primary">Complete</button></td>');
+    newRow.append('<td class="col-md-1 clear-block"><button id = "clear-button" class="btn btn-primary">Clear</button></td>');
+    tbody.append(newRow);
+}
+});
+
 
